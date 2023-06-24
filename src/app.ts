432db3f4dc4483e5b1d13e5d18f8b0d4c2852080
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2023-06-24 18:08:23 +0600

    - add 404 not found route - add helth check route

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,19 @@ app.use(express.json())
 
 app.use("/api/v1", route)
 
+// helth check route
+app.use("/api/v1/helth", (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(200)
+})
+
+// 404 not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next({
+    status: 404,
+    message: "Not found!",
+  })
+})
+
 // Error handling middleware
 interface CustomErr extends ErrorRequestHandler {
   status: number
