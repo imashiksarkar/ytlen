8b6a4c2c0941281e04e90fdf6487a24c69047272
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2023-07-02 00:44:24 +0600

    - deleted test code - cleared unused import

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,6 @@ import route from "./route"
 import errorMiddleware from "./middlewares/error.mw"
 import notFound from "./middlewares/notFound.mw"
 import helth from "./middlewares/helth.mw"
-import Err from "./utils/CustomError"
 
 const app = express()
 
@@ -25,22 +24,6 @@ app.use("/api/v1", route)
 
 // helth check route
 app.use("/api/v1/helth", helth)
-
-// delete later
-// uncaught exception
-app.use("/api/v1/uncaught-exception", (req, res, next) => {
-  throw new Err(400, "uncaught-exception")
-})
-process.on("unhandledRejection", (_error: Err) => {
-  app.use((_req, res, _next) => {
-    res.status(500).json("jjjj")
-  })
-})
-// delete later
-// unhandled promise rejection
-app.use("/api/v1/unhandled-promise-rejection", (_req, _res, _next) => {
-  Promise.reject("unhandled-promise-rejection")
-})
 
 // 404 not found route (Catch All Route)
 app.all("*", notFound)
