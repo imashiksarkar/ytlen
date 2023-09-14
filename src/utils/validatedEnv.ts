38f01d22e5917e415b5557de40d289d162acce23
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2023-09-14 13:39:21 +0600

    - trim env for removing white spaces

--- src/utils/validatedEnv.ts
+++ src/utils/validatedEnv.ts
@@ -3,15 +3,15 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const apiKey = z.string().min(10).parse(process.env.API_KEY)
+const apiKey = z.string().min(10).parse(process.env.API_KEY?.trim())
 
 const nodeEnv = z
   .enum(["development", "production"])
-  .parse(process.env.NODE_ENV)
+  .parse(process.env.NODE_ENV?.trim())
 
 const port = z
   .number()
   .min(4)
-  .parse(parseInt(process.env.PORT || ""))
+  .parse(parseInt(process.env.PORT?.trim() || ""))
 
 export default { apiKey, nodeEnv, port }
