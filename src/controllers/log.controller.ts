230a8068ac9301d9303873d3e41346fc5e617dfe
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2023-11-16 12:36:08 +0600

    - logger fix

--- src/controllers/log.controller.ts
+++ src/controllers/log.controller.ts
@@ -2,11 +2,15 @@ import type { Request, Response } from "express"
 import { Http } from "http-staror"
 import { createReadStream } from "node:fs"
 
-export const getLogs = async (_req: Request, res: Response, next: NextFn) => {
+export const getLogs = (_req: Request, res: Response, next: NextFn) => {
   res.header("Content-Type", "Text/Plain")
   const readStream = createReadStream("logs/info.log")
   readStream.on("error", () =>
-    next(Http.setStatus("InternalServerError").setMessage("Error Reading File"))
+    next(
+      Http.setStatus("InternalServerError").setMessage(
+        "Error Reading File, Check to see if the 'env' is set to production"
+      )
+    )
   )
   return readStream.pipe(res)
 }
